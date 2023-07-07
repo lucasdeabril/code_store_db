c96ad8b49b9d2297e3335759b20c32839341fb2a
lucasdeabril <lucaspassosdesouza08@gmail.com>
2023-07-06 21:16:08 -0400

    rota para conecxão com o banco de dados mySQL iniciada

--- config/routes.js
+++ config/routes.js
@@ -3,4 +3,26 @@ const routes = express.Router()
 
 
 
+let db = [
+    { '1': { Nome: 'Cliente 1', Idade: '20' } },
+    { '2': { Nome: 'Cliente 2', Idade: '20' } },
+    { '3': { Nome: 'Cliente 3', Idade: '20' } }
+  ];
+  
+// buscar dados
+routes.get('/', (req, res) => {
+return res.json(db);
+});
+
+// inserir dados
+routes.post('/add', (req, res)=>{
+const body = req.body
+if(!body)
+return res.status(400).end()
+
+db.push(body)
+return res.json(body)
+})
+  
+
 module.exports = routes
